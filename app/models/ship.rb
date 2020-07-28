6ddb715c5ab7619824a3caecc61b4af0042028d1
Jonathan East <jonathan_east@ymail.com>
2020-07-28 21:29:56 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  SHIPS = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    SHIPS << self
+  end
 end
