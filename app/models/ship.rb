a87490dff2412d98a31dbd37d7e3a7c22579601d
kanu-e <chukwuemekakanu@gmail.com>
2020-08-10 12:19:48 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+   SHIP = []
+
+    def initialize (args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+       SHIP << self
+    end
+
+    def self.clear
+      SHIP.clear
+    end
+
+    def self.all
+        SHIP
+    end
+
+
 end
