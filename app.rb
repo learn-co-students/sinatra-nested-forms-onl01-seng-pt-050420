2a72dd4aa294a55a028783f19dd859fed0fb2bd7
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-08-10 12:14:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+
+    get '/' do
+      erb :new
+    end
+
+    get '/new' do
+      #binding.pry
+      erb :root
+    end
 
   end
 end
